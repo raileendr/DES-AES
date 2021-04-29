3de5050278637088d57117fa8bab47ad0e1c785d
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-04-29 16:08:36 -0700

    pseudocode for setKey

--- AES.cpp
+++ AES.cpp
@@ -1,34 +1,35 @@
 #include "AES.h"
 
-/**
- * Sets the key to use
+/** Sets the key to use
  * @param key - the first byte of this represents whether
  * to encrypt or to decrypt. 00 means encrypt and any other
  * value to decrypt.  Then come the bytes of the 128-bit key
  * (should be 16 of them).
- * @return - True if the key is valid and False otherwise
- */
+ * @return - True if the key is valid and False otherwise */
 bool AES::setKey(const unsigned char* keyArray)
 {
 
-	// TODO: AES implementation of openssl cares about whether
-	// you are encrypting or decrypting when setting the key.
-	// That is, when encrypting you use function AES_set_encrypt_key(...)
-	// and when decrypting AES_set_decrypt_key(...).
-	//
-	// One way to solve this problem is to pass in a 17 byte key, where
-	// the first byte is used to indicate whether we are encrypting or
-	// decrypting. E.g., if the first byte is 0, then use AES_set_encrypt_key(...).
-	// Otherwise, use AES_set_decrypt_key(...).  The rest of the bytes in the
-	// array indicate the 16 bytes of the 128-bit AES key.
-	//
-	// Both functions return 0 on success and other values on faliure.
-	// For documentation, please see https://boringssl.googlesource.com/boringssl/+/2623/include/openssl/aes.h
-	// and aes.cpp example provided with the assignment.
+	// TODO: AES implementation of openssl cares about whether you are encrypting or decrypting when setting the key.
+		// when encrypting, use: AES_set_encrypt_key(...)
+		// when decrypting, use: AES_set_decrypt_key(...)
 
+
+	// if (keyArray[0] == 0){
+			// return AES_set_encrypt_key(AES_KEY, 128, &enc_key)
+			// this returns zero on success and a negative number on error.
+	//	} 
+	//	else {
+			// return AES_set_decrypt_key(AES_KEY, 128, &dec_key)
+			// this returns zero on success and a negative number on error.
+	// }
+
+
+	// NOTE** will be given a 17 byte key, where the first byte is used to indicate whether 
+		// we are encrypting or decrypting. The rest of the bytes in the array indicate the 
+		// 16 bytes of the 128-bit AES key. (only 128-bit AES required for this project)
+	// For documentation, please see https://boringssl.googlesource.com/boringssl/+/2623/include/openssl/aes.h
 
 	return false;
-
 }
 
 /**
