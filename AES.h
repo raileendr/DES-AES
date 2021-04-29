46bcb69afb01a89371a065437ae5e66f9a3bc619
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-04-29 16:06:19 -0700

    Add protected var: "AES_KEY"
    Added a protected variable, the key for AES.
    Also reduce the number of lines comments were taking up to have code block be more concise

--- AES.h
+++ AES.h
@@ -11,46 +11,38 @@
 
 using namespace std;
 
-/**
- * Implements a AES cipher
- */
+/* Implements a AES cipher */
 class AES : public CipherInterface
 {
 	/* The public members */
-public:
+	public:
+		/* The default constructor */
+		AES() {}
 
-	/**
-	 * The default constructor
-	 */
-	AES() {}
 
-	/**
-	 * Sets the key to use
-	 * @param key - the key to use
-	 * @return - True if the key is valid and False otherwise
-	 */
-	virtual bool setKey(const unsigned char* key);
+		/** Sets the key to use
+		 * @param key - the key to use
+		 * @return - True if the key is valid and False otherwise */
+		virtual bool setKey(const unsigned char* key);
 
-	/**
-	 * Encrypts a plaintext string
-	 * @param plainText - the plaintext string
-	 * @return - the encrypted ciphertext string
-	 */
-	virtual unsigned char* encrypt(const unsigned char* plainText);
+		/** Encrypts a plaintext string
+		 * @param plainText - the plaintext string
+		 * @return - the encrypted ciphertext string */
+		virtual unsigned char* encrypt(const unsigned char* plainText);
 
-	/**
-	 * Decrypts a string of cipherText
-	 * @param ciphertext - the cipherText
-	 * @return - the plaintext
-	 */
-	virtual unsigned char* decrypt(const unsigned char* cipherText);
+		/** Decrypts a string of cipherText
+		 * @param ciphertext - the cipherText
+		 * @return - the plaintext */
+		virtual unsigned char* decrypt(const unsigned char* cipherText);
 
 
 	/* The protected members */
-protected:
+	protected:
 
 	//TODO: declare the AES key
+	unsigned char AES_KEY;
+	
 };
 
-
 #endif
+
